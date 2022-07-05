6e71d288503e0d52699146a4d83e37182aefa6e1
kazi basnet <basnetkazi2@gmail.com>
2022-07-05 17:32:15 +0545

    initial commit

---
+++ pages/teamplace/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Teamplace = () => {
+  return (
+    <>
+	<h1 className = "text-red-900 text-9xl">hello! you are in Teamplace</h1>
+  </>
+  )
+}
+
+export default Teamplace
