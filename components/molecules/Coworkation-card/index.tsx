e1edfd8a674c34805279db0563f9ba5a6f08ebb2
kaushal.basnet <kaushal.basnet@admin01012s-MacBook-Air.local>
2022-07-18 17:29:52 +0545

    desktop design complete

--- components/molecules/Coworkation-card/index.tsx
+++ components/molecules/Coworkation-card/index.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
-const CoworkationCard = () => {
-  return (
-    <div className="w-[394px] h-[436px] bg-[#000000] opacity-70 rounded-[10px] mix-blend-multiply p-[35px] text-white bg-[url('/img/imageSection.png')]">CoworkationCard</div>
-  )
+import React from "react";
+interface Props {
+  card: number;
+  title: string;
+  facilities: string;
+  desc: string;
+  img: any;
 }
-export {CoworkationCard}
+const CoworkationCard = ({ card, title, facilities, desc, img }: Props) => {
+  console.log(img);
+  return (
+    <div
+      className={`w-[394px] h-[436px] bg-[#000000] opacity-70 rounded-[10px] mix-blend-multiply p-[35px] text-white bg-[url('/img/${img}')]`}
+    >
+      <div className="flex flex-col gap-y-[10px]">
+        <p className="text-[#F95F00] leading-[110%] text-[18px] font-medium">
+          case <span className="text-[40px] font-semibold">{card}</span>
+        </p>
+        <div className="border-dashed border-b-[1px] pb-7 mb-[26px] border-white w-full">
+          <p className="text-[22px] font-bold leading-[140%] ">{title}</p>
+        </div>
+
+        <p className="text-[18px] font-bold leading-[140%]">{facilities}</p>
+        <p className="text-[16px] leading-7">{desc}</p>
+      </div>
+    </div>
+  );
+};
+export { CoworkationCard };
--- pages/coworkation/index.tsx
+++ pages/coworkation/index.tsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import { CoworkationCard } from '../../components'
+import React from "react";
+import { CoworkationCard } from "../../components";
+import COWORKATION from "../../coworkationApi";
 
 const Coworkation = () => {
   return (
-    <div className='pt-[75px] pb-20 px-24'>
+    <div className="bg-[#F5F5F5] h-screen">
+      <div className="pt-[25px] pb-20 bg-[#FFFFFF] h-[751px] w-full ">
         <div>
-        <h2 className='pb-[10px] text-[#1B1B1B] leading-[140%] text-[30px] text-bold'>企業の合宿・研修・ワーケーション手配に関するお悩みを解決できます！</h2>
-        <p className='text-[#2E2E2E] leading-[34px] text-[18px] text-medium'>コワーケーション.comは、企業の合宿やオフサイトミーティング時に最適な滞在施設、研修・体験コンテンツ、そして必要な機材準備まで一括で手配するコンシェルジュ型サービスです。コワーケーションの準備をすべてお任せください。</p>
+          <h2 className="text-[#F5F5F5] text-[160px] leading-[110%] font-bold relative">
+            POINT
+          </h2>
+          <div className="absolute top-[75px] px-24">
+            <h2 className="pb-[10px] text-[#1B1B1B] leading-[140%] text-[30px] font-bold">
+              企業の合宿・研修・ワーケーション手配に関するお悩みを解決できます！
+            </h2>
+            <p className="text-[#2E2E2E] leading-[34px] text-[18px] font-medium">
+              コワーケーション.comは、企業の合宿やオフサイトミーティング時に最適な滞在施設、研修・体験コンテンツ、そして必要な機材準備まで一括で手配するコンシェルジュ型サービスです。コワーケーションの準備をすべてお任せください。
+            </p>
+          </div>
         </div>
-      <div className='mt-[50px]'>
-       <CoworkationCard/> 
+        <div className="flex gap-x-9 mt-[50px]  px-24">
+          {COWORKATION.map((elem: any, id) => (
+            <CoworkationCard
+              key={id}
+              img={elem.img}
+              card={elem.card}
+              title={elem.title}
+              facilities={elem.facilites}
+              desc={elem.desc}
+            />
+          ))}
+        </div>
       </div>
-        </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Coworkation
+export default Coworkation;
