9966318a2c653be2fd10b19c8bca54a7f072054c
kaushal.basnet <kaushal.basnet@admin01012s-MacBook-Air.local>
2022-07-19 13:55:58 +0545

    coworkation desktop design complete

--- components/molecules/Coworkation-card/index.tsx
+++ components/molecules/Coworkation-card/index.tsx
@@ -10,11 +10,16 @@ const CoworkationCard = ({ card, title, facilities, desc, img }: Props) => {
   console.log(img);
   return (
     <div
-      className={`w-[394px] h-[436px] bg-[#000000] opacity-70 rounded-[10px] mix-blend-multiply p-[35px] text-white bg-[url('/img/${img}')]`}
+      className={
+        "w-[394px] h-[436px] rounded-[10px]  p-[35px] text-white relative"
+      }
+      // Tailwind CSS doesn't really deal with dynamic data, so use inline css
+      style={{ backgroundImage: `url(/img/${img})` }}
     >
-      <div className="flex flex-col gap-y-[10px]">
+      <div className=" bg-[#000000] mix-blend-multiply opacity-70 absolute inset-0 rounded-[10px]"></div>
+      <div className="flex flex-col gap-y-[10px] relative">
         <p className="text-[#F95F00] leading-[110%] text-[18px] font-medium">
-          case <span className="text-[40px] font-semibold">{card}</span>
+          Case <span className="text-[40px] font-semibold">{card}</span>
         </p>
         <div className="border-dashed border-b-[1px] pb-7 mb-[26px] border-white w-full">
           <p className="text-[22px] font-bold leading-[140%] ">{title}</p>
